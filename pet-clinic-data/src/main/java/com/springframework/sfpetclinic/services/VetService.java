edbf11aaf21b59e3bf24fde1cbda4951d8ef4a73
aztecus1 <alaaeddine.ouerfelli@esprit.tn>
2021-10-08 18:58:01 +0100

    Refractor services and add map Based Services

--- pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/springframework/sfpetclinic/services/VetService.java
@@ -2,10 +2,8 @@ package com.springframework.sfpetclinic.services;
 
 import com.springframework.sfpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+
+public interface VetService extends CrudService<Vet, Long> {
+
 }
